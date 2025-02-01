2f7b973c185c01eee07a3afbc0440d2cfd492e99
Dhaneesh7 <dhaneeshvj5@gmail.com>
2025-02-01 14:40:23 +0530

    Update Signup.js

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -2,6 +2,7 @@
 import { useNavigate } from "react-router-dom";
 
 const Signup = ({ setUser }) => {
+const [name,setName]=useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
@@ -20,6 +21,13 @@ const Signup = ({ setUser }) => {
     <div className="flex items-center justify-center min-h-screen">
       <form className="p-6 border rounded" onSubmit={handleSignup}>
         <h2 className="text-2xl font-bold mb-4">Signup</h2>
+        <input
+          className="border p-2 w-full mb-2"
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
         <input
           className="border p-2 w-full mb-2"
           type="email"
